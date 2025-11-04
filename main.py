5e40ecece3659097b2a23e9870f6c29b1a83247a
zsun3471-cyber <zsun3471@tcusd.net>
2025-11-04 21:29:38 +0000

    hihi

--- main.py
+++ main.py
@@ -6,8 +6,23 @@ def clear_terminal():
         _ = os.system('cls')
     else:
         _ = os.system('clear')
+
+def gambling():
+    chance = random.random()
+    return round(chance,2)
 clear_terminal()
 
-
 money = 0
-chance = None
+
+while True:
+    mainmenu = input("would you like to mine for ores? Y/N")
+    if mainmenu.upper() == "Y":
+        chance = gambling()
+        if chance >= 0.9:
+            print('you found diamonds!')
+            money += chance * 100000
+            print(money)
+        else:
+            print('found nothing, aww...')
+    else:
+        break
