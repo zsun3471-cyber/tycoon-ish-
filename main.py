30f9472496be5b393d883c3849dfc3d8bf3cfa89
zsun3471-cyber <zsun3471@tcusd.net>
2025-11-04 21:18:18 +0000

    added terminal clearing

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import os
+import random
 
+def clear_terminal():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
+clear_terminal()
+
+
+money = 0
+chance = None
