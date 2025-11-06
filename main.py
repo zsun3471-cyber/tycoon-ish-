4358f0f2adf833660ef8d22448b135f2934f1fa2
zsun3471-cyber <zsun3471@tcusd.net>
2025-11-06 16:42:35 +0000

    rewrote some stuff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 
 def clear_terminal():
     if os.name == 'nt':
@@ -10,19 +11,19 @@ def clear_terminal():
 def gambling():
     chance = random.random()
     return round(chance,2)
+
+def typewriter(text, delay = 0.05):
+    for i in text:
+        print(i, end = "", flush=True)
+        time.sleep(delay) 
+
 clear_terminal()
 
+shoplist = ["2x Luck", "Iron Pickaxe", "Golden Pickaxe", "Diamond Pickaxe", "Galvanized Square Steel Pickaxe"]
 money = 0
 
 while True:
-    mainmenu = input("would you like to mine for ores? Y/N")
-    if mainmenu.upper() == "Y":
-        chance = gambling()
-        if chance >= 0.9:
-            print('you found diamonds!')
-            money += chance * 100000
-            print(money)
-        else:
-            print('found nothing, aww...')
-    else:
-        break
+    mainmenu = input(typewriter("Welcome to the main menu! What would you like to do?\nYou can go mining, shopping for tools, and gamble!!!!111\n"))
+    if mainmenu.lower() == "shop" or "shop" in mainmenu.lower():
+        typewriter("Welcome to the shop. You can buy stuff in here! (no way right!!??)")
+        typewriter("\nYou can buy: " + ", ".join(shoplist)+ " ")
